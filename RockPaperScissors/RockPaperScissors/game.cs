dd06d5443ef0b04aa3052efb5c8549e8d095f7ce
Ginkobaloba <Dramattick1@gmail.com>
2016-08-10 10:14:38 -0500

    First commit

---
+++ RockPaperScissors/RockPaperScissors/game.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public class game
+    {
+        player playerOne;
+        player playerTwo;
+        string Player;
+        public void RunGame()
+        {
+            
+
+            Console.WriteLine("Welcome, to a simple game of Rock Paper Scissors. Player One what is your name?:");
+            Player = Console.ReadLine();
+            this.playerOne = new player(Player);
+            Console.WriteLine("Hello, {0} thank you for playing.");
+            Console.WriteLine("Welcome, Player Two What is your Name?:");
+            Player = Console.ReadLine();
+            this.playerTwo = new player(Player);
+           
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors/player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    public class player
+    {
+        string PlayerName;
+        int weapon;
+
+        public player(string Player)
+        {
+            this.PlayerName = Player;
+            this.weapon = 50;
+        }
+    }
+}
